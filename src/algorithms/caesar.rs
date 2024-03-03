7526d5b5de711ae415ca52183cecf9f7a1e9687c
michirakara <naotokonagai@gmail.com>
2024-03-03 12:45:03 -0800

    implemented caesar cipher

---
+++ src/algorithms/caesar.rs
@@ -0,0 +1,17 @@
+pub fn encrypt(text: String) -> String {
+    let mut to_return: String = String::new();
+    for chr in text.bytes() {
+        if (65..=90).contains(&chr) {
+            let encrypted: &str = &(((chr - 65 + 3) % 26 + 65) as char).to_string();
+            to_return += encrypted;
+        }
+        else if (97..=122).contains(&chr) {
+            let encrypted: &str = &(((chr - 97 + 3) % 26 + 97) as char).to_string();
+            to_return += encrypted;
+        }
+        else {
+            to_return += &(chr as char).to_string();
+        }
+    }
+    to_return
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod algorithms;
+use structopt::StructOpt;
+
+//TODO: エラーを実装する
+#[derive(StructOpt, Debug)]
+/// encryption & decryption tool
+enum Opt {
+    /// caeser cipher
+    Caesar {
+        mode: String,
+        text: String,
+        #[structopt(short = "h", long)]
+        help: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    match Opt::from_args() {
+        Opt::Caesar {
+            mode,
+            text,
+            help: _,
+        } => {
+            if mode == "enc" {
+                println!("Result: {}", algorithms::caesar::encrypt(text));
+            } else if mode == "dec" {
+                todo!("decryption mode WIP");
+            }
+        }
+    }
 }
