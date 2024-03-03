ac32fed436cc426df7eaf7925ad08bf533cc22ac
michirakara <naotokonagai@gmail.com>
2024-03-03 13:22:21 -0800

    changed implementation of caesar cipher to use rot cipher

--- src/algorithms/caesar.rs
+++ src/algorithms/caesar.rs
@@ -1,30 +1,6 @@
 pub fn encrypt(text: String) -> String {
-    let mut to_return: String = String::new();
-    for chr in text.bytes() {
-        if (65..=90).contains(&chr) {
-            let encrypted: &str = &(((chr - 65 + 3) % 26 + 65) as char).to_string();
-            to_return += encrypted;
-        } else if (97..=122).contains(&chr) {
-            let encrypted: &str = &(((chr - 97 + 3) % 26 + 97) as char).to_string();
-            to_return += encrypted;
-        } else {
-            to_return += &(chr as char).to_string();
-        }
-    }
-    to_return
+    crate::algorithms::rot::encrypt(3, text)
 }
 pub fn decrypt(text: String) -> String {
-    let mut to_return: String = String::new();
-    for chr in text.bytes() {
-        if (65..=90).contains(&chr) {
-            let encrypted: &str = &(((chr - 65 + 23) % 26 + 65) as char).to_string();
-            to_return += encrypted;
-        } else if (97..=122).contains(&chr) {
-            let encrypted: &str = &(((chr - 97 + 23) % 26 + 97) as char).to_string();
-            to_return += encrypted;
-        } else {
-            to_return += &(chr as char).to_string();
-        }
-    }
-    to_return
+    crate::algorithms::rot::decrypt(3, text)
 }
