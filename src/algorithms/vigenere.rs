29646c6f912b44ddbfeefe0a058120c0a6be717b
michirakara <naotokonagai@gmail.com>
2024-03-03 18:07:35 -0800

    implemented vigenère cipher

---
+++ src/algorithms/vigenere.rs
@@ -0,0 +1,44 @@
+pub fn encrypt(key: String, text: String) -> String {
+    // key must be all uppercase
+    let mut to_return: String = String::new();
+
+    let mut key_index:usize=0;
+    for chr in text.bytes() {
+        if (65..=90).contains(&chr) {
+            let step: u8 = key.chars().nth(key_index).unwrap() as u8 - 65;
+            let encrypted: &str = &(((chr - 65 + step) % 26 + 65) as char).to_string();
+            to_return += encrypted;
+            key_index=(key_index+1)%key.len();
+        } else if (97..=122).contains(&chr) {
+            let step: u8 = key.chars().nth(key_index).unwrap() as u8 - 65;
+            let encrypted: &str = &(((chr - 97 + step) % 26 + 97) as char).to_string();
+            to_return += encrypted;
+            key_index=(key_index+1)%key.len();
+        } else{
+            to_return += &(chr as char).to_string();
+        }
+    }
+    to_return
+}
+pub fn decrypt(key: String, text: String) -> String {
+    // key must be all uppercase
+    let mut to_return: String = String::new();
+
+    let mut key_index:usize=0;
+    for chr in text.bytes() {
+        if (65..=90).contains(&chr) {
+            let step: u8 = key.chars().nth(key_index).unwrap() as u8 - 65;
+            let encrypted: &str = &(((chr - 65 + 26 - step) % 26 + 65) as char).to_string();
+            to_return += encrypted;
+            key_index=(key_index+1)%key.len();
+        } else if (97..=122).contains(&chr) {
+            let step: u8 = key.chars().nth(key_index).unwrap() as u8 - 65;
+            let encrypted: &str = &(((chr - 97 +26 - step) % 26 + 97) as char).to_string();
+            to_return += encrypted;
+            key_index=(key_index+1)%key.len();
+        } else{
+            to_return += &(chr as char).to_string();
+        }
+    }
+    to_return
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod algorithms;
-use structopt::StructOpt;
 use rand::Rng;
+use structopt::StructOpt;
 
 //TODO: エラーを実装する
 #[derive(StructOpt, Debug)]
@@ -21,35 +21,94 @@ enum Opt {
         mode: String,
         /// text to encrypt/decrypt
         text: String,
-        /// key for cipher
+        /// key for cipher. automatically generates random key when not specified
         key: Option<u8>,
     },
+    /// vigenère cipher
+    Vigenere{
+        /// available modes: enc, dec
+        mode: String,
+        /// text to encrypt/decrypt
+        text: String,
+        /// key for cipher. automatically generates random key when not specified
+        key: Option<String>
+    }
 }
 
 fn main() {
     match Opt::from_args() {
+        // Caesar cipher
         Opt::Caesar { mode, text } => {
             if mode == "enc" {
-                println!("Result: {}", algorithms::caesar::encrypt(text));
+                // encrypt
+                let cipher_text: String = algorithms::caesar::encrypt(text);
+                // output
+                println!("Result: {}", cipher_text);
             } else if mode == "dec" {
-                println!("Result: {}", algorithms::caesar::decrypt(text));
+                // decrypt
+                let plain_text: String = algorithms::caesar::decrypt(text);
+                // output
+                println!("Result: {}", plain_text);
             } else {
+                // when mode is not `enc` or `dec`
                 unimplemented!("error: unavailable mode");
             }
         }
-        Opt::Rot { mode, text , key} => {
+        // ROT-n cipher
+        Opt::Rot { mode, text, key } => {
             if mode == "enc" {
                 // generate key when unspecified
                 let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
-                let final_key:u8=key.unwrap_or(rng.gen_range(1..26));
+                let final_key: u8 = key.unwrap_or(rng.gen_range(1..26));
                 // encrypt
+                let cipher_text: String = algorithms::rot::encrypt(final_key, text);
+                // output
                 println!("Key: {}", final_key);
-                println!("Result: {}", algorithms::rot::encrypt(final_key, text));
+                println!("Result: {}", cipher_text);
             } else if mode == "dec" {
                 // decrypt
+                let plain_text: String = algorithms::rot::decrypt(key.unwrap(), text);
+                // output
                 println!("Key: {}", key.unwrap());
-                println!("Result: {}", algorithms::rot::decrypt(key.unwrap(), text));
+                println!("Result: {}", plain_text);
             } else {
+                // when mode is not `enc` or `dec`
+                unimplemented!("error: unavailable mode");
+            }
+        }
+        // Vigenere cipher
+        Opt::Vigenere { mode, text, key } => {
+            if mode=="enc" {
+                let mut final_key:String;
+                if key==None{
+                    // generate key
+                    let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
+                    
+                    let key_length:i32=rng.gen_range(1..=10);
+                    final_key=String::new();
+                    for _ in 0..key_length{
+                        let rand:u8=rng.gen_range(65..=90);
+                        let next_key:&str=&(rand as char).to_string();
+                        final_key+=next_key;
+                    }
+                }else{
+                    // when key is specified
+                    final_key=key.unwrap().to_uppercase();
+                }
+                // encrypt
+                let cipher_text: String=algorithms::vigenere::encrypt(final_key.clone(),text);
+                // output
+                println!("Key: {}",final_key);
+                println!("Result: {}", cipher_text);
+            } else if mode=="dec"{
+                // decrypt
+                let final_key=key.unwrap().to_uppercase();
+                let plain_text: String = algorithms::vigenere::decrypt(final_key.clone(),text);
+                // output
+                println!("Key: {}", final_key);
+                println!("Result: {}", plain_text);
+            }else{
+                // when mode is not `enc` or `dec`
                 unimplemented!("error: unavailable mode");
             }
         }
